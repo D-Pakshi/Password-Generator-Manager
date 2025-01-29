058918e12a77c4fe9e47e7cc39af21c7941bf658
D-Pakshi <dipi.tara@gmail.com>
2025-01-29 11:30:16 +0530

    updated code for retrieval of passwords on user request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 def join(arr, separator=''):
@@ -6,35 +7,90 @@ def join(arr, separator=''):
     return separator.join(map(str, arr))
 
 
+#---------------------------------------------storage of data on request of user-----------------------------------------------------------------
 
-# Character sets
-lower = list("abcdefghijklmnopqrstuvwxyz")
-upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-numbs = list("0123456789")
-symbs = list("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-
-# Correct combined list
-combined = lower + upper + numbs + symbs
-
-print("\nWelcome USER to your password generator!\n")
-
-website = input("Enter NAME of the website: ")
-link = input("Enter LINK: ")
-passdigits = int(input("Enter how many DIGITS of password required: "))
-
-# Initialize the password with one of each character type
-strongpass = (
-    random.choice(lower) +
-    random.choice(upper) +
-    random.choice(numbs) +
-    random.choice(symbs)
-)
-
-for i in range(0,passdigits-4):
-    strongpass+= random.choice(combined)
+def store(): #storing the user inputs
     
-hashpass = join(random.sample(strongpass,passdigits), "")
+    lower = list("abcdefghijklmnopqrstuvwxyz")
+    upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    numbs = list("0123456789")
+    symbs = list("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+
+    combined = lower + upper + numbs + symbs
+
+    website = input("Enter NAME of the website: ").lower()
+    username = input("Enter USERNAME: ")
+    passdigits = int(input("Enter how many DIGITS of password required: "))
+
+    # Initialize the password with one of each character type
+    strongpass = (
+        random.choice(lower) +
+        random.choice(upper) +
+        random.choice(numbs) +
+        random.choice(symbs)
+    )
+
+    for i in range(0,passdigits-4):
+        strongpass+= random.choice(combined)
+    
+    hashpass = join(random.sample(strongpass,passdigits), "") #final password
+    
+    #saving data as dict to string in the txt file
+    newdict = {
+        "website"  : website,
+        "username" : username,
+        "password" : hashpass
+    }
+
+    data_str=json.dumps(newdict)
 
 
-with open("password.txt", "a") as f:
-    f.write(f"{website} | {link} | {hashpass}\n")
+    with open("password.txt", "a") as f:
+        f.write(f"{data_str}\n")
+
+    print(f"Save Successful....\nYour new generated {passdigits} digits password is ----> {hashpass} <----\n")
+
+#---------------------------------------------retrieval of data on request of user-----------------------------------------------------------------
+
+def retrieve():
+    request = input("\nWhich website? : ").lower()
+
+    with open("password.txt") as f:
+        passlist = f.readlines()
+    
+    for dict in passlist:
+        current_dict = json.loads(dict)
+        if request == current_dict["website"]:
+            print(f"\nFor {request}:\n")
+            print(f"USERNAME --> {current_dict["username"]}")
+            print(f"PASSWORD --> {current_dict["password"]}\n")
+            return
+        
+    print("ERROR 404: Website NOT FOUND")
+
+#-----------------------------------------------------------User interface---------------------------------------------------------------------------
+
+print("\nWelcome USER to your Password Generator & Manager!\n")
+
+def UI():
+    
+    print("\n [1] GENERATE and save new password")
+    print("\n [2] RETRIEVE username & password\n")
+
+    user_input=input()
+
+    if user_input=="1":
+        store()
+    elif user_input=="2":
+        retrieve()
+    else :
+        print("\nPLEASE USE ONE OF THE TWO OPTIONS")
+        UI()
+
+
+UI()
+
+
+
+
+
